0447edcc5228b128974ed1fa0f6c29ab97e88bc0
AlessandroBertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-05-14 11:33:18 +0200

    main file

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.Scraper import GZScraper
+from src.OntologyBuilder import main, prova_html
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    main()
+
+    # prova_html()
+
+    # gz = GZScraper()
+    #
+    # # print(gz.recipes_information)
