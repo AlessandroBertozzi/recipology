452e07551b4bc9783ed6455cb5c2f7969000c097
AlessandroBertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-06-01 17:46:03 +0200

    add all base queries for retrieve reasoning result

--- src/sparql_query.py
+++ src/sparql_query.py
@@ -3,9 +3,6 @@ from owlready2 import *
 import random
 
 
-def main():
-    onto = get_ontology("../static/prova_2.owl").load()
-
 
 
 
@@ -23,22 +20,78 @@ def query_for_selection_individuals():
     dict_with_results = dict()
     for record in query:
         if record[0].name not in dict_with_results:
-            dict_with_results[record[0].name] = list()
-        dict_with_results[record[0].name].append((record[1].hasIngredient[0].name, record[1].hasQuantity[0]))
+            dict_with_results[record[0].name] = {"Ingredients": list()}
+        dict_with_results[record[0].name]["Ingredients"].append((record[1].hasIngredient[0].name, record[1].hasQuantity[0]))
 
-    print(dict_with_results)
+    return dict_with_results
 
 
 def query_for_categories():
-    pass
+    onto = get_ontology("../static/prova_2.owl").load()
+
+    categories = list(default_world.sparql("""
+            PREFIX rc: <http://www.semanticweb.org/veronicabertozzi/ontologies/2022/3/untitled-ontology-45#>
+            SELECT  ?subject 
+            WHERE { ?subject rdfs:subClassOf rc:ByCategory .
+            ?subject rdf:type owl:Class .
+
+            }"""))
+
+    tmp_dict = dict()
+
+
+
+    for category in categories:
+        for name_recipe in category[0].instances():
+            if category[0] not in tmp_dict:
+                tmp_dict[name_recipe.name] = category[0].name
+
+    return tmp_dict
+
+
+def combination_category_informations_of_individuals():
+    onto = get_ontology("../static/prova_2.owl").load()
+
+    recipes = query_for_selection_individuals()
+    categories = query_for_categories()
+
+    for recipe, category in categories.items():
+
+        recipes[recipe]["Category"] = category
+
+    return recipes
 
 
 
 def query_for_inferred_categories():
-    # owlready2.JAVA_EXE = "C:\\Program Files\\Java\\jre1.8.0_333\\bin\\java.exe"
-    # sync_reasoner_hermit(infer_property_values=True)
-    pass
+    onto_1 = get_ontology("../static/prova_2.owl").load()
+
+    recipe_inferred_categories = list(default_world.sparql("""
+            PREFIX rc: <http://www.semanticweb.org/veronicabertozzi/ontologies/2022/3/untitled-ontology-45#>
+            SELECT  ?subject 
+            WHERE { ?subject rdfs:subClassOf rc:Recipe_by_category .
+            }"""))
+    general_dict = combination_category_informations_of_individuals()
+
+    onto_2 = get_ontology("../data/inferred_ontology/recipology_inferred.owl").load()
+
+    with onto_2:
+        for category in recipe_inferred_categories:
+            for recipe in category[0].instances():
+                if "inferred_categories" not in general_dict[recipe.name].keys():
+                    general_dict[recipe.name]["Inferred_categories"] = list()
+                general_dict[recipe.name]["Inferred_categories"].append(category[0].name)
+    return general_dict
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    query_for_selection_individuals()
+    pass
